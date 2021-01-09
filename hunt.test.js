c5491c559a7ba4efa9ad1ebde27a2665194f53f6
kmitcham <kmitcham@gmail.com>
2021-01-08 20:20:21 -0800

    more hunt cases

--- hunt.test.js
+++ hunt.test.js
@@ -31,6 +31,68 @@ test('injury case', () =>{
     expect(gameState["population"][playername]['food']).toBe(0)
 });
 
+test('injury edge case', () =>{
+    var gameState = {
+          "seasonCounter": 1,
+          "gameTrack": {
+            "veldt": 1,
+            "forest": 1,
+            "marsh": 1,
+            "hills": 1
+          },
+          "name": "flounder-tribe",
+          "population": {
+            "Hunter1": {
+              "gender": "female",
+              "spearhead": 0,
+              "name": "Hunter1",
+              "food":0,
+              "profession": "hunter"
+            }
+          },
+          "currentLocationName": "marsh",
+          "round": "work"
+    }
+    var playername = "Hunter1"
+    var player = gameState["population"][playername]
+    output = lib.hunt(playername, player, 5, gameState)
+    expect(output).toMatch('Injury!');
+    expect(gameState["population"][playername]["isInjured"]).toBeTruthy()
+    expect(gameState["gameTrack"]["marsh"]).toBe(2)
+    expect(gameState["population"][playername]['food']).toBe(0)
+});
+test('injury avoided with strong case', () =>{
+    var gameState = {
+          "seasonCounter": 1,
+          "gameTrack": {
+            "veldt": 1,
+            "forest": 1,
+            "marsh": 1,
+            "hills": 1
+          },
+          "name": "flounder-tribe",
+          "population": {
+            "Hunter1": {
+              "gender": "female",
+              "spearhead": 0,
+              "name": "Hunter1",
+              "strength":"strong",
+              "food":0,
+              "profession": "hunter"
+            }
+          },
+          "currentLocationName": "marsh",
+          "round": "work"
+    }
+    var playername = "Hunter1"
+    var player = gameState["population"][playername]
+    output = lib.hunt(playername, player, 5, gameState)
+    expect(output).toMatch('No game');
+    expect(output).toMatch('strong');
+    expect(gameState["population"][playername]["isInjured"]).not.toBeTruthy()
+    expect(gameState["gameTrack"]["marsh"]).toBe(2)
+    expect(gameState["population"][playername]['food']).toBe(0)
+});
 test('spear fail', () =>{
     var gameState = {
           "seasonCounter": 1,
@@ -121,3 +183,4 @@ test('threshold, no spear', () =>{
     expect(gameState["population"][playername]['worked']).toBeTruthy()
     expect(gameState["gameTrack"]["marsh"]).toBe(2)
 });
+
