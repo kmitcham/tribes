fc6cb15c3bc94913ee85ee9c0ad70051c39b2707
kmitcham <kmitcham@gmail.com>
2021-02-17 12:42:35 -0800

    need these libs too

---
+++ children.js
@@ -0,0 +1,56 @@
+const utillib = require("./util.js");
+const guardlib = require("./guardCode.js");
+
+module.exports.showChildren =  (children, filterName="" ) =>{
+		response = ''
+		childNames = Object.keys(children)
+		response = 'There are '+childNames.length+' children in total. \n'
+		mine = 0 
+		var notPrintedNewAdultHeader = true;
+		var notStartedMiddleChildren = true;
+		var notStartedYoungChildren = true;
+        var notStartedUnborn = true;
+		if (filterName){
+			response += 'The descendants of '+filterName+' are:\n'
+		}
+		for (childName in children) {
+			child = children[childName]
+			if (filterName && !(child.mother == filterName || child.father == filterName) ) {
+				continue
+			}
+			if (child.dead){
+				//response += '('+childName+' is dead)\n';
+				// skip the dead
+			} else {
+				if (child.age < 0 && notStartedUnborn){
+					response += '-----> Unborn <-----\n'
+					notStartedUnborn = false;
+                } else if (child.age < 4 && notStartedYoungChildren ){
+					response += '-----> Young Children <-----\n'
+					notStartedYoungChildren = false;
+				} else if (child.age < 24 && notStartedMiddleChildren){
+					response += '-----> Children <-----\n'
+					notStartedMiddleChildren = false;
+				}	else if (child.age >= 24 && notPrintedNewAdultHeader){
+					response += '-----> New Adults <-----\n'
+					notPrintedNewAdultHeader = false;
+				}
+				response += '('+childName+':'+child.gender
+				response += ' years:'+((child.age)/2)
+				if (child.newAdult){
+					response += ' Full grown!'
+				} else {
+					response += ' needs '+(2-child.food)+' food'
+				} 
+				response += ' parents:'+child.mother+'+'+child.father
+				if (child.age < 24 ){
+					response += ' guardValue:'+ utillib.round(guardlib.findGuardValueForChild(childName, population, children))
+				}
+				if (child.babysitting){
+					response += ' watching:'+child.babysitting+' '
+				}
+				response += ')\n'
+			}
+        } 
+		return response
+}
