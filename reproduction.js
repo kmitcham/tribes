7a94d27d7b913487fff7aa43c4385fc9da23699e
kmitcham <kmitcham@gmail.com>
2021-03-31 20:15:49 -0700

    keep this draft, as some tests of the lists work

--- reproduction.js
+++ reproduction.js
@@ -1,4 +1,4 @@
-
+const util = require("./util.js");
 
 
 function eligibleMates(name, population, debug=false){
@@ -37,42 +37,234 @@ function eligibleMates(name, population, debug=false){
 }
 module.exports.eligibleMates = eligibleMates;
 
-function validMate(inviter, target, gameState){
+function matingObjections(inviter, target){
+    response = "";
+    if (inviter.isPregnant){
+        response += inviter.name+" is pregnant.\n"
+    }
+    if (target.isPregnant){
+        response += target.name+" is pregnant.\n"
+    }
+    if (inviter.gender == target.gender){
+        response += inviter.name+" is the same gender as "+target.name+".\n"
+    }
+    return response;
+}
+module.exports.matingObjections = matingObjections;
+
+function handleReproductionList(actorName, args, listName, gameState, bot){
+    actor = util.personByName(actorName, gameState);
+    population = gameState.population
+    errors = []
+    list = []
+    for (rawTargetName of args){
+        localErrors = "";
+        if (rawTargetName == "!pass"){
+            console.log("Detected pass "+args.indexOf(rawTargetName)+"  "+(args.length -1));
+            if (args.indexOf(rawTargetName) != (args.length -1)){
+                errors.push("Values after '!pass' must be removed.\n")
+                console.log(" adding an error ")
+            }
+            break;
+        }
+        targetName = util.removeSpecialChars(rawTargetName)
+        target = util.personByName(targetName, gameState);
+        if (target){
+            localErrors+= matingObjections(actor, target)
+        } else {
+            localErrors+= rawTargetName+" is not in the tribe.\n"
+        }
+        if (localErrors != ""){
+            errors.push(...localErrors)
+        } else {
+            list.push(targetName)
+        }
+    }
+    // if just !pass, or no args, or something perverse not thought of
+    if (list.length == 0 && errors.length == 0 ){
+        errors.push("No valid items for the "+listName+" list.\n");
+    }
+    if (errors.length > 0){
+        console.log("consentlist has errors")
+        for (error of errors){
+            util.messagePlayerName(actorName, error, gameState, bot)
+        }
+        // clean up message?
+        return -1 * errors.length;
+    }
+    actor[listName] = list;
+    util.messagePlayerName(actorName, "Setting your "+listName+" list to:"+list, gameState, bot)
+    return 0;
+}
+module.exports.handleReproductionList = handleReproductionList;
+
+
+function invite(msg, gameState, bot){
+    author = msg.author
+    actorName = util.removeSpecialChars(author.username)
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    handleInvite(actorName, args, gameState, bot )
+    globalMatingCheck(gameState, bot);
 
 }
+module.exports.invite = invite;
 
-function invite(args, gameState, bot){
 
+function handleInvite(actorName, args, gameState, bot){
+    actor = util.personByName(actorName, gameState);
+    population = gameState.population
+    errors = []
+    invites = []
+    for (rawTargetName of args){
+        localErrors = "";
+        if (rawTargetName == "!pass"){
+            if (args.indexOf(rawTargetName != (args.length -1))){
+                localErrors+= "Values after '!pass' will be ignored.\n"
+            }
+            break;
+        }
+        targetName = util.removeSpecialChars(targetName)
+        target = util.personByName(targetName, gameState);
+        if (target){
+            localErrors+= matingObjections(actor, target)
+        } else {
+            localErrors+= rawTargetName+" is not in the tribe.\n"
+        }
+        if (localErrors != ""){
+            errors.push(localErrors)
+        } else {
+            invites.push(targetName)
+        }
+    }
+    // if just !pass, or no args, or something perverse not thought of
+    if (invites.length == 0 && errors.length == 0 ){
+        errors.push("No valid items for the invite list.\n");
+    }
+    if (errors.length > 0){
+        for (error of errors){
+            author.message(error)
+        }
+        // clean up message?
+        return -1 * errors.length;
+    }
+    actor.invites = invites;
+    author.message("Setting your invite list to:"+invites)
+    globalMatingCheck(gameState, bot);
+    return;
 }
-/* 
-invite <target1> [ optional additional targets]
-    actor.inviteTargets = [target1, target2]
-    validateMatingTargets(source, [targets])
-    globalMatingCheck()
-*/
-/*
-consent <target1> [ optional additional targets]
-    validateMatingTargets(source, [targets])
-    actor.consentList = [target1, target2...]
-    globalMatingCheck()
-*/
-/*
- decline <target1> [additional targets]
-    if any <target1> not in tribe
-        err, exit
-    actor.declineList = [target1, target2...]
-    globalMatingCheck()       
-*/
-/*
-pass
-    // NOTE: a) pass is ALWAYS public.  b) You CAN secret invite, get a consent, then publicly skip.
-    mark actor 'cannotInvite'
-    delete actor.inviteList
-    announce pass in public channel
-    if (tribe has everyone cannotInvite)
-        end mating
-        announce pregnancies
+module.exports.handleInvite = handleInvite;
 
+function consent(msg, gameState, bot){
+    author = msg.author
+    actorName = util.removeSpecialChars(author.username)
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    return handleConsent(actorName, args, gameState, bot )
+}
+module.exports.consent = consent;
+
+    
+function handleConsent(actorName, args, gameState, bot){
+    actor = util.personByName(actorName, gameState);
+    population = gameState.population
+    errors = []
+    consents = []
+    for (rawTargetName of args){
+        localErrors = "";
+        targetName = util.removeSpecialChars(targetName)
+        target = util.personByName(targetName, gameState);
+        if (target){
+            localErrors+= matingObjections(actor, target)
+        } else {
+            localErrors+= rawTargetName+" is not in the tribe.\n"
+        }
+        if (localErrors != ""){
+            errors.push(localErrors)
+        } else {
+            consents.push(targetName)
+        }
+    }
+    // if just !pass, or no args, or something perverse not thought of
+    if (consents.length == 0 && errors.length == 0 ){
+        errors.push("No valid items for the consent list.\n");
+    }
+    if (errors.length > 0){
+        for (error of errors){
+            author.message(error)
+        }
+        // clean up message?
+        return -1 * errors.length;
+    }
+    actor.consents = consents;
+    author.message("Setting your consent list to:"+consents)
+    globalMatingCheck(gameState, bot);
+    return;
+}
+module.exports.handleConsent = handleConsent;
+
+function decline(msg, gameState, bot){
+    author = msg.author
+    actorName = util.removeSpecialChars(author.username)
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    return handleDecline(author.username, args, gameState, bot )
+}
+module.exports.decline = decline;
+   
+function handleDecline(actorName, args, gameState, bot){
+    actor = util.personByName(actorName, gameState);
+    population = gameState.population
+    errors = []
+    consents = []
+    for (rawTargetName of args){
+        localErrors = "";
+        targetName = util.removeSpecialChars(targetName)
+        target = util.personByName(targetName, gameState);
+        if (target){
+            localErrors+= matingObjections(actor, target)
+        } else {
+            localErrors+= rawTargetName+" is not in the tribe.\n"
+        }
+        if (localErrors != ""){
+            errors.push(localErrors)
+        } else {
+            consents.push(targetName)
+        }
+    }
+    // if just !pass, or no args, or something perverse not thought of
+    if (consents.length == 0 && errors.length == 0 ){
+        errors.push("No valid items for the decline list.\n");
+    }
+    if (errors.length > 0){
+        for (error of errors){
+            author.message(error)
+        }
+        // clean up message?
+        return -1 * errors.length;
+    }
+    actor.consents = consents;
+    author.message("Setting your decline list to:"+consents)
+    globalMatingCheck(gameState, bot);
+    return;
+}
+module.exports.handleDecline = handleDecline;
+
+function pass(msg, gameState, bot){
+    author = msg.author
+    actor = util.personByName(author.username, gameState);
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    actor.cannotInvite = true;
+    util.messageChannel(author.username+" passes on mating this turn.", gameState, bot)
+    globalMatingCheck(gameState, bot);
+    return;
+}
+module.exports.pass = pass;
+
+function skip(){}
+/*
+// how does the lib know if someone is a referee?
 skip <arg>
     if actor is not chief or arg != actor 
 		err, exit
@@ -82,8 +274,77 @@ skip <arg>
 
 
 */
+function globalMatingCheck(gameState, bot){
+    console.log("global mating not yet implented")
+    allDone = true;
+    actionableInvites = true;
+    while (actionableInvites){
+        actionableInvites = false;
+        sexList = keys(population)
+        for (personName of sexList){
+            person = util.personByName(personName)
+            index = sexList.indexOf(personName)
+            if (person.cannotInvite) {
+                //remove person from sexList
+                sexList.slice(index, index+1)
+                continue
+            }
+            if (person.inviteTargets.length > 0) {
+                targetName = inviteTargets[0]
+                if (targetName == "!pass"){
+                    person.cannotInvite = true
+                    util.messageChannel(personName+" is passing on mating this round.")
+                    delete person.inviteTargets
+                    sexList.slice(index, index+1)
+                    continue
+                }
+                target = personForName(targetName)
+                if (target.consentList.includes(personName)){
+                    // spawn should message the people
+                    spawn(target, person, gameState, bot)
+                    person.cannotInvite = true
+                    delete person.inviteTargets
+                    sexList.slice(index, index+1)
+                    continue
+                } else if (target.declineList.includes(personNamne)){
+                    util.messagePlayerName(personName, targetName+" declines your invitation.", gameState, bot)
+                    util.messagePlayerName(targetName, personName+" flirts with you.", gameState, bot)
+                    person.inviteTargets.shift()
+                    // can't lose your invite power just because of rejection
+                    if (person.inviteTargets.length > 0) {
+                        actionableInvites = true
+                    }
+                    allDone = false
+                } else {
+                    // this will get spammy, if the function is called every time anyone updates.
+                    util.messagePlayerName(targetName, personName+" has invited you to mate.", gameState, bot)
+                    util.messagePlayerName(personName, targetName+" considers your invitation.", gameState, bot)
+                    sexList.slice(index, index+1)
+                    allDone = false
+                }
+            } else {
+                // person has no invites pending
+                sexList.slice(index, index+1)
+                allDone = false
+            }
+        }
+    }
+    // allDone = true should also mean sexList is empty.
+    if (allDone){
+        for (personName in population){
+            if (person.hiddenPregnant){
+                util.messageChannel(person.name+ " has been blessed with a child.", gameState, bot)
+                person.isPregnant = true 
+                delete person.hiddenPregnant
+            }
+            delete person.inviteList  // this should already be empty or done.
+        }
+        util.messageChannel("There are no outstanding invitations.  Time for chance.", gameState, bot)
+    }
+    
+}
 /* 
-globalMatingCheck()
+globalMatingCheck(gamestate, bot)
     allDone = true;
     actionableInvites = true;    
     while (actionableInvites){
@@ -111,6 +372,7 @@ globalMatingCheck()
                     continue
                 } else if (target.declineList.includes(personNamne)){
                     message person (target declines your invitation)
+                    message target (person tried to flirt with you)
                     person.inviteTargets.shift()
                     // can't lose your invite power just because of rejection
                     if (person.inviteTargets.length > 0) {
@@ -136,12 +398,11 @@ globalMatingCheck()
         message the channel repro is over, time for chance
         for person in tribe{
             if (person.hiddenPregnant){
-                announce pregnancy
+                announce pregnancy in channel
                 person.isPregnant = true 
                 delete person.hiddenPregnant
             }
             delete person.inviteList  // this should already be empty or done.
-            // I think I lean towards KEEPING
         }
     }
 }
@@ -151,7 +412,10 @@ spawn
     <existing dice roll code>
 	if (roll1+roll2 > 9 && ! (mother.hiddenPregnant || mother.isPregnant) ){
         addChild
-        mother.hiddenPregnant = true
+        if (gameData.secretMating)
+            mother.hiddenPregnant = true
+        else 
+            mother.isPregnant
     }
     if gameData.secretMating
         message the parents about feelings (show each their own roll)
--- reproduction.test.js
+++ reproduction.test.js
@@ -54,3 +54,132 @@ test("makes a list", () =>{
   response = reproLib.eligibleMates("p1", gameState.population, false);
   expect(response).toBe("p2, p3, p4")
 });
+
+test("valid mates", () =>{
+  var p1 ={
+              "name": "p1",
+              "gender": "female",
+              "activity": "gather"
+          };
+  var p2 = {  "name": "p2",
+            "gender": "male"
+          };
+  response = reproLib.matingObjections(p1,p2)
+  expect(response).toBe("")
+});
+test("same gender mates", () =>{
+  var p1 ={
+              "name": "p1",
+              "gender": "female",
+              "activity": "gather"
+          };
+  var p2 = {  "name": "p2",
+            "gender": "female"
+          };
+  response = reproLib.matingObjections(p1,p2)
+  expect(response).toBe("p1 is the same gender as p2.\n");
+});
+
+test("pregnant mates", () =>{
+  var p1 ={
+              "name": "p1",
+              "gender": "female",
+              "isPregnant": true,
+              "activity": "gather"
+          };
+  var p2 = {  "name": "p2",
+            "gender": "male"
+          };
+  var response = reproLib.matingObjections(p1,p2)
+  expect(response).toBe("p1 is pregnant.\n")
+  var response = reproLib.matingObjections(p2,p1)
+  expect(response).toBe("p1 is pregnant.\n")
+});
+test("Mating objections happy path", ()=> {
+  p1 = {
+    "name": "p1",
+    "gender": "female"
+}
+  p2 = {
+    "name": "p2",
+    "gender": "male"
+  }
+  var response = reproLib.matingObjections(p1,p2);
+  expect(response).toBe("");
+  
+})
+test("Mating objections fail", ()=> {
+  p1 = {
+    "name": "p1",
+    "gender": "female",
+    "isPregnant":"someKid"
+  }
+  p2 = {
+    "name": "p2",
+    "gender": "male"
+  }  
+  p3 = {
+    "name": "p3",
+    "gender": "male"
+  }
+  var response = reproLib.matingObjections(p2,p3);
+  expect(response).toBe("p2 is the same gender as p3.\n")
+  response = reproLib.matingObjections(p1,p2);
+  expect(response).toBe("p1 is pregnant.\n");
+  response = reproLib.matingObjections(p2,p1);
+  expect(response).toBe("p1 is pregnant.\n");
+  response = reproLib.matingObjections(p1,p1);
+  expect(response).toBe("p1 is pregnant.\np1 is pregnant.\np1 is the same gender as p1.\n");
+})
+test("make a consentList, happy path", () =>{
+  var gameState = {
+      "population": {
+          "p1":{
+              "name": "p1",
+              "gender": "female"
+          },
+          "p2":{
+            "name": "p2",
+            "gender": "male"
+          }
+          , "p3":{
+            "name": "p3",
+            "gender": "male"
+          }
+          , "p4":{
+              "name": "p4",
+              "gender": "male"
+            }
+       },
+      "round": "reproduction"
+  }
+  //function handleReproductionList(actorName, args, listName, gameState, bot){
+  response = reproLib.handleReproductionList("p1", ["p2", "p4"],"consentList", gameState, {})
+  expect(gameState["population"]["p1"]["consentList"]).toStrictEqual(["p2","p4"])
+});
+test("make a consentList, with pass", () =>{
+  var gameState = {
+      "population": {
+          "p1":{
+              "name": "p1",
+              "gender": "female"
+          },
+          "p2":{
+            "name": "p2",
+            "gender": "male"
+          }
+          , "p3":{
+            "name": "p3",
+            "gender": "male"
+          }
+          , "p4":{
+              "name": "p4",
+              "gender": "male"
+            }
+       },
+      "round": "reproduction"
+  }
+  //function handleReproductionList(actorName, args, listName, gameState, bot){
+  response = reproLib.handleReproductionList("p1", ["p2", "p4", "!pass", "p3"],"consentList", gameState, {})
+  expect(response).toBe(-1)
+});
