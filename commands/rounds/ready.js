e4a8767b430ad75636fb3cb8fece4acfcf8da8cf
kmitcham <kmitcham@gmail.com>
2026-04-04 22:37:56 -0700

    fix ref commands, and tweak ready message

--- commands/rounds/ready.js
+++ commands/rounds/ready.js
@@ -18,35 +18,11 @@ module.exports = {
 };
 
 function onCommand(displayName, gameState) {
-  var message = 'Nobody seems ready for much of anything right now.';
+  var message = 'Nobody seems ready to work since it is not the work round.';
   if (gameState.workRound) {
     message =
       'People available to work: ' +
       worklib.listReadyToWork(gameState.population);
-  }
-  // this may just be an artifact of non-secret mating?
-  if (gameState.reproductionRound) {
-    if (gameState.reproductionList && gameState.reproductionList[0]) {
-      message =
-        'The mating invitation order is ' +
-        gameState.reproductionList.join(', ') +
-        '\n';
-      var cleanName = gameState.reproductionList[0];
-      if (cleanName.indexOf('(') > 0) {
-        startParen = cleanName.indexOf('(');
-        cleanName = cleanName.substring(0, startParen);
-      }
-      message +=
-        'Available partners: ' +
-        reproLib.eligibleMates(cleanName, gameState.population);
-      for (personName in population) {
-        member = pop.memberByName(personName, gameState);
-        if (member.invite) {
-          message +=
-            '\n' + personName + ' is awaiting a response from ' + member.invite;
-        }
-      }
-    }
   }
   text.addMessage(gameState, displayName, message);
   return;
